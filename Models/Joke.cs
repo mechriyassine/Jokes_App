5a464e61e38be4ad6c2a64879c24dc95698816a9
mechriyassine <mechriyassine78@gmail.com>
2024-03-17 23:03:18 +0100

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
